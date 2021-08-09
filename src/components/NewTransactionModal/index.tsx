04bb15495470534b79b09424b8da5f14a57a6019
nullCommit <allangeek@gmail.com>
2021-08-09 10:19:35 -0300

    Insert transactions in summary elements

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,5 @@
 import { FormEvent, useState, useContext } from 'react';
 import Modal from 'react-modal';
-import { api } from '../../services/api';
 import { TransactionsContext } from '../../TransactionsContext';
 
 import closeImg from '../../assets/close.svg';
@@ -25,15 +24,22 @@ export function NewTransactionModal({
   const [category, setCategory] = useState('');
   const [type, setType] = useState('deposit');
 
-  function handleCreateNewTransaction(event: FormEvent) {
+  async function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    createTransaction({
+    await createTransaction({
       title,
       amount,
       category,
       type,
     });
+
+    setTitle('');
+    setAmount(0);
+    setCategory('');
+    setType('deposit');
+
+    onRequestClose();
   }
 
   return (
